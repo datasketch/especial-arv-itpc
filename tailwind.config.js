13ca6edf6dab990e72f37e493707d580a9fd824e
David Daza <03dazal@gmail.com>
2020-06-01 10:44:56 -0500

    setup inicial

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'baby-powder': '#FEFCF8',
+        'bottle-green': '#1E6356',
+        'charleston-green': '#222526',
+        'cosmic-latte': '#FDF6E7',
+        'orange-soda': '#E85736',
+        'orange-yellow': '#FCC22D',
+        alabaster: '#F4EDE4',
+        fulvous: '#E28A00',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
